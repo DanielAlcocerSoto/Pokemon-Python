c92a2d07bf117a567975626b345e17ff632fab41
DanielAlcocerSoto <daniel.das.estudios@gmail.com>
2018-02-26 01:16:02 +0100

    More coments, some change in selector
    changed visible time of selector
    add more constants in seelct_config
    In the last Commit added a setup.py!

--- Game/display/image.py
+++ Game/display/image.py
@@ -2,7 +2,14 @@
 # -*- coding: utf-8 -*-
 
 """
+Module that contains the some generic classes, useful for the implementation of
+classes related to the visualization of elements on the screen.
 
+It contains the following classes:
+
+	Image
+	Background
+	Display
 """
 
 # Local imports
@@ -18,29 +25,84 @@ __author__  = 'Daniel Alcocer (daniel.alcocer@est.fib.upc.edu)'
 
 
 """
-
+	Generic class to display a image.
 """
-class Image(pygame.sprite.Sprite):
+class Image:
 	def __init__(self, image, top_left_location):
-		pygame.sprite.Sprite.__init__(self)
+		"""
+		Args:
+			image (class:'pygame.Surface'): The image to display in the screen.
+			top_left_location (set:'int','int'): The top left location of the
+												 'image' in the screen.
+
+		Action:
+			Create a image that will be displayed in a screen in the
+			'top_left_location' position.
+		"""
 		self._image = image
 		self._location = top_left_location
 
-	def display(self,SCREEN):
+	"""
+		Funcion to display this image.
+	"""
+	def display(self, SCREEN):
+		"""
+		Args:
+			SCREEN (class:'pygame.Surface'): The current screen of the window
+											 where to display the image.
+
+		Action:
+			Display the image in SCREEN.
+		"""
 		SCREEN.blit(self._image, self._location)
 
+"""
+	Class to display a Background image.
+"""
 class Background(Image):
 	def __init__(self, image_file, top_left_location = [0,0]):
+		"""
+		Args:
+			image (class:'pygame.Surface'): The image to display in the screen.
+			top_left_location (set:'int','int'): The top left location of the
+												 'image' in the screen.
+
+		Action:
+			Create a image that will be displayed in a screen in the
+			'top_left_location' position.
+		"""
 		image = load_background(image_file)
 		factor = Display_Config['BACKGROUND_SCALE']
 		final_image =  pygame.transform.scale(image, scale_bg(image.get_size()))
 		location = scale_bg(top_left_location)
 		Image.__init__(self,final_image,location)
 
+"""
+	Generic class to display several displayables objects.
+"""
 class Display:
 	def __init__(self, visualize_items):
+		"""
+		Args:
+			visualize_items ('list of obj'): A list of object with a 'display'
+											 function (p.e. Image).
+
+		Action:
+			Create a object with several displayable items.
+		"""
 		self._visualize_items = visualize_items
 
+	"""
+		Funcion to display this several items.
+	"""
 	def display(self, SCREEN):
+		"""
+		Args:
+			SCREEN (class:'pygame.Surface'): The current screen of the window
+											 where to display the image.
+
+		Action:
+			Display several items in SCREEN.
+		"""
 		for surface in self._visualize_items:
 			surface.display(SCREEN)
--- Game/display/selection/selector.py
+++ Game/display/selection/selector.py
@@ -2,7 +2,12 @@
 # -*- coding: utf-8 -*-
 
 """
+Module that contains the Selector class.
+This class allows to create a selector to get a selection from the screen.
 
+It contains the following class:
+
+	Selector
 """
 
 # Local imports
@@ -14,22 +19,43 @@ from Game.display.utils_display import pair_mult_num, scale, scale_bg, transalte
 # 3rd party imports
 import pygame
 
-__version__ = '0.7'
+__version__ = '0.9'
 __author__  = 'Daniel Alcocer (daniel.alcocer@est.fib.upc.edu)'
 
 
 """
-
+    Class to display a selector of butttons and get the index of the buttton
+    selected.
 """
 class Selector:
     def __init__(self, select_move = True, display_selector = True):
+        """
+            Args:
+                select_move ('bool'): Indicate if select moves or pokemons.
+                display_selector ('bool'): Indicates whether the selector
+                                           should be displayed.
+            Action:
+                Create a selector able to move between the different buttons and
+                obtain the index of the selected button.
+        """
         self._select_move = 1 if select_move else 2
         self.display_selector = display_selector
         self._shift = (0,Display_Config['BATTLE_SIZE'][1]+Display_Config['LOG_SIZE'][1])
         self.pos = [0,0]
-        self.set_location()
+        self._time_init = pygame.time.get_ticks()
+        self._set_location()
 
-    def set_location(self):
+    """
+        Class function to set the location of the selector.
+    """
+    def _set_location(self):
+        """
+            Args: -
+
+            Action:
+            	Set the position of this selector in the correct loction depending
+                on 'self.pos'.
+        """
         name = 'SELECTOR_L_FILE' if self.pos[1] == 2 else 'SELECTOR_FILE'
         image = load_cell(Directory[name])
         self._image = pygame.transform.scale(image, scale_bg(image.get_size()))
@@ -41,28 +67,52 @@ class Selector:
             width,height = Select_Config['POS_CANCEL']
         location = transalte((width,height), self._shift)
         self._location = scale_bg(location)
+        self._time_init = pygame.time.get_ticks()
 
+    """
+        Moves the selector to the left.
+    """
     def move_to_left(self):
-        self.pos[0]=max(self.pos[0]-1,0)
-        self.set_location()
+        old_pos = list(self.pos)
+        self.pos[0] = max(self.pos[0]-1,0)
+        if old_pos != self.pos: self._set_location()
 
+    """
+        Moves the selector to the right.
+    """
     def move_to_right(self):
-        self.pos[0]=min(self.pos[0]+1,1)
-        self.set_location()
+        old_pos = list(self.pos)
+        self.pos[0] = min(self.pos[0]+1,1)
+        if old_pos != self.pos: self._set_location()
 
+    """
+        Moves the selector up.
+    """
     def move_to_up(self):
-        self.pos[1]=max(self.pos[1]-1,0)
-        self.set_location()
+        old_pos = list(self.pos)
+        self.pos[1] = max(self.pos[1]-1,0)
+        if old_pos != self.pos: self._set_location()
 
+    """
+        Moves the selector down.
+    """
     def move_to_down(self):
-        self.pos[1]=min(self.pos[1]+1,self._select_move)
-        self.set_location()
+        old_pos = list(self.pos)
+        self.pos[1] = min(self.pos[1]+1,self._select_move)
+        if old_pos != self.pos: self._set_location()
 
+    """
+        Get the index of the botton currently selected.
+    """
     def get_selection(self):
         return min(self.pos[1]*2+self.pos[0],4)
 
+    """
+        Funcion to display this object.
+    """
     def display(self,SCREEN):
         if self.display_selector:
-            x = pygame.time.get_ticks()/1000
-            if x-int(x)<0.7:
+            stripe = (1000*Select_Config["SELECTOR_DISPLAY_STRIPE"])
+            delta_time = (pygame.time.get_ticks() - self._time_init)/stripe
+            if delta_time-int(delta_time)<Select_Config["SELECTOR_DISPLAY_TIME"]:
                 SCREEN.blit(self._image, self._location)
